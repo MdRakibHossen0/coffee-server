4eb23884869a4ee947aeda90e3b727a9be94a987
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-05-19 00:01:29 +0600

    done work

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
     await client.connect();
 
     const coffeesCollection = client.db("coffeeDB").collection("coffees");
+    const usersCollection = client.db("coffeeDB").collection("users");
 
     app.get("/coffees", async (req, res) => {
       // const cursor = coffeesCollection.find();
@@ -51,9 +52,9 @@ async function run() {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      const updatedcoffee = req.body;
+      const updatedCoffee = req.body;
       const updateDoc = {
-        $set: updatedcoffee,
+        $set: updatedCoffee,
       };
       const result = await coffeesCollection.updateOne(
         filter,
@@ -67,6 +68,40 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //user related APIs
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const userProfile = req.body;
+      console.log(userProfile);
+      const result = await usersCollection.insertOne(userProfile);
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+     const {email,lastSignInTime} = req.body
+     const filter = {email:email}
+     const updatedDoc = {
+       $set: {
+         lastSignInTime: lastSignInTime,
+       },
+     };
+     const result = await usersCollection.updateOne(filter,updatedDoc)
+     res.send(result)
+
+      
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
